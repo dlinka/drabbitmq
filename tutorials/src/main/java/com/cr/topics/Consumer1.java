79b0c6b02738f3251688529d66e90251e363407a
dlinka <dlinka.cg@gmail.com>
2020-05-06 21:11:01 +0800

    格式化

--- tutorials/src/main/java/com/cr/topics/Consumer1.java
+++ tutorials/src/main/java/com/cr/topics/Consumer1.java
@@ -5,7 +5,6 @@ import com.cr.Queue;
 import com.cr.RabbitMQConnection;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.DeliverCallback;
 
 import java.io.IOException;
 
@@ -18,9 +17,7 @@ public class Consumer1 {
             channel.queueDeclare(Queue.TOPICS_QUEUE_1.name(), false, false, false, null);
             channel.queueBind(Queue.TOPICS_QUEUE_1.name(), Exchange.TOPICS_EXCHANGE.name(), "1.#");
             channel.queueBind(Queue.TOPICS_QUEUE_1.name(), Exchange.TOPICS_EXCHANGE.name(), "3.#");
-            DeliverCallback deliverCallback = (consumerTag, delivery) -> System.out.println(new String(delivery.getBody()));
-            channel.basicConsume(Queue.TOPICS_QUEUE_1.name(), true, deliverCallback, consumerTag -> {
-            });
+            channel.basicConsume(Queue.TOPICS_QUEUE_1.name(), true, (consumerTag, delivery) -> System.out.println(new String(delivery.getBody())), consumerTag -> {});
         } catch (IOException e) {
             e.printStackTrace();
         }
