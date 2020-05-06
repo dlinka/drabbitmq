a25d454dcbb0a28d22d61196575802ca297c4170
dlinka <dlinka.cg@gmail.com>
2020-05-06 21:12:44 +0800

    格式化

--- tutorials/src/main/java/com/cr/publishSubscribe/Consumer2.java
+++ tutorials/src/main/java/com/cr/publishSubscribe/Consumer2.java
@@ -5,7 +5,6 @@ import com.cr.Queue;
 import com.cr.RabbitMQConnection;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.DeliverCallback;
 
 import java.io.IOException;
 
@@ -16,9 +15,7 @@ public class Consumer2 {
             Channel channel = connection.createChannel();
             channel.queueDeclare(Queue.PUBLISH_SUBSCRIBE_QUEUE_2.name(), false, false, true, null);
             channel.queueBind(Queue.PUBLISH_SUBSCRIBE_QUEUE_2.name(), Exchange.PUBLISH_SUBSCRIBE_EXCHANGE.name(), "");
-            DeliverCallback deliverCallback = (consumerTag, delivery) -> System.out.println(new String(delivery.getBody()));
-            channel.basicConsume(Queue.PUBLISH_SUBSCRIBE_QUEUE_2.name(), true, deliverCallback, consumerTag -> {
-            });
+            channel.basicConsume(Queue.PUBLISH_SUBSCRIBE_QUEUE_2.name(), true, (consumerTag, delivery) -> System.out.println(new String(delivery.getBody())), consumerTag -> {});
         } catch (IOException e) {
             e.printStackTrace();
         }
